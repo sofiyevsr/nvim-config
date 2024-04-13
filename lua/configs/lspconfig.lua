527f6fd0293459c5b42623123de641e2b73afef7
Rasul Sofiyev <sofiyevsr@gmail.com>
2024-04-13 17:28:49 +0400

    avoid clipboard interfering with system clipboard

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "gopls", "eslint" }
+local servers = { "html","cssls", "astro", "tsserver", "gopls", "eslint" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
